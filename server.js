65fa6ca6ea002bed7a0f6828a0761caa8ebc3285
Pooja <poojasri.s@skematix.in>
2026-02-20 17:54:59 +0530

    updates

--- server.js
+++ server.js
@@ -5,34 +5,28 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
-
 // load env
 dotenv.config();
-
 
 // connect database
 connectDB();
 
-
 // create app
 const app = express();
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 // routes
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("JWT Authentication on live...");
+  res.send("Hello World!");
 });
 
 // server
 app.listen(process.env.PORT, () => {
-
   console.log(`Server running on port ${process.env.PORT}`);
-
 });
+
